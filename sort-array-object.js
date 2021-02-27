163c06779b955e11ed59d23f8b7352399e873e27
nitsaw09 <niteshghuge09@gmail.com>
2021-02-27 16:54:26 +0530

    Update sort-array-object.js

--- sort-array-object.js
+++ sort-array-object.js
@@ -1,17 +1,19 @@
 const users = [
     {id: 1, name: "Antony"},
-    {id: 2, name: "Fred"},
+    {id: 2, name: "fred"},
     {id: 3, name: ""},
     {id: 4, name: "Word"},
     {id: 5, name: "Bailey"}
 ];
 
 const sortData = (itemList, field, order) => {
-    const sortItem = itemList.sort((a, b) => {
-       	if(a[field] < b[field]) {
+    const sortItem = itemList.sort((a, b) => { 
+        const val1 = a[field].toLowerCase();
+        const val2 = b[field].toLowerCase()
+       	if(val1 < val2) {
          return order === "ASC" ? -1 : 1;   
        	}
-         if ( a[field] > b[field] ){
+         if ( val1 > val2){
            return order === "ASC" ? 1 : -1;
          }
        return 0;
