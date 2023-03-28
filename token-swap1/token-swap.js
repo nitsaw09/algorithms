c1ef97f51989071a97ad16527e22830e7a05b7e2
nitsaw09 <niteshghuge09@gmail.com>
2023-03-28 10:28:56 +0530

    token swap optimize

--- token-swap1/token-swap.js
+++ token-swap1/token-swap.js
@@ -1,28 +1,25 @@
 function computeTokenSwaps(n, ratesList, q, queriesList) {
   const rates = {};
   const allTokens = {};
-
+  let token1;
   // Parse token pairs and rates into a rates object
   for (let i = 0; i < n; i++) {
     const [t1, t2, r] = ratesList[i].split(", ");
     const rate = parseFloat(r);
     if (!rates[t1]) rates[t1] = {};
+    if (!token1) token1 = t1;
     rates[t1][t2] = rate;
     allTokens[t1] = true;
     allTokens[t2] = true;
   }
 
-  // Compute all conversion rates using Floyd's algorithm
-  let Token1;
-  for (const k in allTokens) {
-    for (const i in allTokens) {
-      for (const j in allTokens) {
-        if (rates[i] && rates[i][k] && rates[k] && rates[k][j]) {
-          const newRate = rates[i][k] * rates[k][j];
-          if (!rates[i][j] || newRate > rates[i][j]) {
-            rates[i][j] = newRate;
-            Token1 = i;
-          }
+  for (const t1 in allTokens) {
+    if (rates[token1] && !rates[token1][t1]) {
+      for (const t2 in allTokens) {
+        if (rates[token1] && rates[token1][t2] && rates[t2] && rates[t2][t1]) {
+          rates[token1][t1] = rates[token1][t2] * rates[t2][t1]
+        } else if (rates[token1] && rates[token1][t2] && rates[t1] && rates[t1][t2]) {
+          rates[token1][t1] = rates[token1][t2] * rates[t1][t2]
         }
       }
     }
@@ -36,10 +33,10 @@ function computeTokenSwaps(n, ratesList, q, queriesList) {
     const amount = parseFloat(amt);
     if (rates[t1] && rates[t1][t2]) {
         conversionRate = rates[t1][t2];
-    } else if (rates[t1] && rates[t2][t1]) {
+    } else if (rates[t2] && rates[t2][t1]) {
         conversionRate = 1/rates[t2][t1];
-    } else {
-        conversionRate = 1/rates[Token1][t1] * 1/rates[Token1][t2]
+    } else if (rates[token1] && rates[token1][t1] && rates[token1][t2]) {
+        conversionRate = 1/rates[token1][t1] * 1/rates[token1][t2]
     }
     
     if (conversionRate !== undefined) {
@@ -53,7 +50,7 @@ function computeTokenSwaps(n, ratesList, q, queriesList) {
 
 // Example usage
 const n = 3;
-const ratesList = ["TokenA, TokenB, 1.2", "TokenB, TokenC, 0.003", "TokenA, TokenD, 0.0000005"];
+const ratesList = ["TokenA, TokenB, 1.2", "TokenB TokenC, 0.003", "TokenA, TokenD, 0.0000005"];
 const q = 4;
-const queriesList = ["TokenA, TokenC, 10000", "TokenB, TokenA, 10000", "TokenD, TokenB, 10000", "TokenC, TokenD, 10000"];
+const queriesList = ["TokenC, TokenD, 10000", "TokenA, TokenC, 10000", "TokenB, TokenA, 10000", "TokenD, TokenB, 10000"];
 computeTokenSwaps(n, ratesList, q, queriesList);
